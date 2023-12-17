2dd8df2c3dac8ca512da7f0be568969f4ef3081d
maxADev <maximealaphilippe12@gmail.com>
2023-12-17 23:05:56 +0100

    Comment deletion codacy fix

--- public/assets/script/script.js
+++ public/assets/script/script.js
@@ -1,47 +1,53 @@
-document.addEventListener("DOMContentLoaded", (event) => {
-    let deletePost = document.getElementsByClassName('delete-post');
-    let deletePostClose = document.getElementById('delete-post-close');
+document.addEventListener("DOMContentLoaded", () => {
+    let deletePost = document.getElementsByClassName("delete-post");
+    let deletePostClose = document.getElementById("delete-post-close");
     if (deletePost && deletePostClose) {
         for (var i = 0; i < deletePost.length; i++) {
-            deletePost[i].addEventListener('click', modalDeletePost, false);
+            deletePost[i].addEventListener("click", modalDeletePost, false);
         }
-    
-        deletePostClose.addEventListener('click', modalDeletePostClose, false);
+        deletePostClose.addEventListener("click", modalDeletePostClose, false);
     }
 
-    let commentPost = document.getElementsByClassName('delete-comment');
-    let deleteCommentClose = document.getElementById('delete-comment-close');
+    let commentPost = document.getElementsByClassName("delete-comment");
+    let deleteCommentClose = document.getElementById("delete-comment-close");
     if (commentPost && deleteCommentClose) {
-        for (var i = 0; i < commentPost.length; i++) {
-            commentPost[i].addEventListener('click', modalDeleteComment, false);
+        for (var a = 0; a < commentPost.length; i++) {
+            commentPost[a].addEventListener("click", modalDeleteComment, false);
         }
-
-        deleteCommentClose.addEventListener('click', modalDeleteCommentClose, false);
+        deleteCommentClose.addEventListener("click", modalDeleteCommentClose, false);
     }
 });
 
-function modalDeletePost()
-{
+/**
+ * @func modalDeletePost
+ */
+function modalDeletePost() {
     let postId = this.dataset.postId;
     document.getElementById("post-deletion-container").style.display = "block";
     document.getElementById("confirm-post-deletion").href = "post-deletion-"+postId;
 }
 
-function modalDeletePostClose()
-{
+/**
+ * @func modalDeletePostClose
+ */
+function modalDeletePostClose() {
     document.getElementById("post-deletion-container").style.display = "none";
     document.getElementById("confirm-post-deletion").href = "";
 }
 
-function modalDeleteComment()
-{
+/**
+ * @func modalDeleteComment
+ */
+function modalDeleteComment() {
     let commentId = this.dataset.commentId;
     document.getElementById("comment-deletion-container").style.display = "block";
     document.getElementById("confirm-comment-deletion").href = "comment-deletion-"+commentId;
 }
 
-function modalDeleteCommentClose()
-{
+/**
+ * @func modalDeleteCommentClose
+ */
+function modalDeleteCommentClose() {
     document.getElementById("comment-deletion-container").style.display = "none";
     document.getElementById("confirm-comment-deletion").href = "";
 }
